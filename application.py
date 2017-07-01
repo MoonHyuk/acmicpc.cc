e75dba567cca3be89d160e2eff63cd53dcdff161
MoonHyuk <godmoon00@naver.com>
2017-07-02 04:04:34 +0900

    Check user is in DB

--- application.py
+++ application.py
@@ -1,20 +1,18 @@
 import os
 import urllib.request
+import datetime
 
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 from models import db
-
+from models import User
 
 application = Flask(__name__)
 application.config.from_object(os.environ['APP_SETTINGS'])
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(application)
 
-
-from models import User
-
-
+# define header for urllib request
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) ' \
              'Chrome/58.0.3029.110 Safari/537.36'
 hds = {'User-Agent': user_agent}
@@ -31,6 +29,11 @@ def is_boj_user(user_id):
         return True
 
 
+def update_profile(user):
+    url = "https://www.acmicpc.net/user/" + user.boj_id
+
+
+
 @application.route('/')
 def render_index():
     return render_template("index.html")
@@ -42,7 +45,17 @@ def search_user():
     if not is_boj_user(user_id):
         return render_template("index.html", id=user_id, err=True)
     else:
-        return render_template("user.html")
+        if not User.query.filter_by(boj_id=user_id).scalar():
+            print("no")
+            user = User(boj_id=user_id)
+            db.session.add(user)
+            db.session.commit()
+
+        user = User.query.filter_by(boj_id=user_id).first()
+        if user.update_time is None or (datetime.datetime.now() - user.update_time).days > 0:
+            update_profile(user)
+
+        return render_template("user.html", user=user)
 
 if __name__ == "__main__":
     application.run()
